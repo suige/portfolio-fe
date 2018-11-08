204d96dc9cf80e8c01de7b48b99270ca4f1751b0
kaori <suige787@live.jp>
2018-11-08 15:11:06 -0800

    :sparkles: Add Skill section

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="about">
     <h1>Kaori is ...</h1>
-
+    <skills></skills>
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'About',
-  };
+  import { Component, Vue } from 'vue-property-decorator';
+  import Skills from '@/components/Skills.vue';
+
+  @Component({
+    components: {
+      Skills,
+    },
+  })
+  export default class About extends Vue {}
 </script>

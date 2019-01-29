3468e7984bf55da4b042fd5f32a80fa4b7060c63
kaori <suige787@live.jp>
2019-01-28 18:01:24 -0800

    Delete global navigation effect

--- src/components/GlobalNavigation.vue
+++ src/components/GlobalNavigation.vue
@@ -4,25 +4,16 @@
       <router-link to="/"><logo/></router-link>
       <ul>
         <li>
-          <button @mouseover="onMouseover" @mouseleave="onMouseleave" @click="onClick('works')">Works</button>
+          <button @click="onClick('works')">Works</button>
         </li>
         <!--<li>-->
           <!--<button @mouseover="onMouseover" @mouseleave="onMouseleave" @click="onClick('about')">AboutMe</button>-->
         <!--</li>-->
         <li>
-          <button @mouseover="onMouseover" @mouseleave="onMouseleave" @click="onClick('contact')">Contact</button>
+          <button @click="onClick('contact')">Contact</button>
         </li>
       </ul>
     </nav>
-    <transition name="fade">
-      <div class="navBackground" v-show="isMouseover"></div>
-    </transition>
-    <transition name="fade-eye">
-      <div class="eyes" v-show="isMouseover">
-        <div class="eye"></div>
-        <div class="eye"></div>
-      </div>
-    </transition>
   </div>
 </template>
 
@@ -37,18 +28,7 @@
   })
   export default class GlobalNavigation extends Vue {
 
-    private isMouseover: boolean = false;
-
-    public onMouseover(): void {
-      this.isMouseover = true;
-    }
-
-    public onMouseleave(): void {
-      this.isMouseover = false;
-    }
-
     public onClick(name: string): void {
-      this.isMouseover = false;
       this.$router.push(name);
     }
   }
@@ -112,122 +92,5 @@
     &:hover {
       z-index: 101;
     }
-  }
-
-  .navBackground {
-    opacity: .9;
-    background: $font-color;
-    position: fixed;
-    width: 100vw;
-    height: 100vh;
-    top: 0;
-    left: 0;
-    z-index: 100;
-  }
-
-  .eyes {
-    position: fixed;
-    bottom: 100px;
-    left: 100px;
-    z-index: 101;
-  }
-
-  .eye {
-    width: 30px;
-    height: 42px;
-    border-radius: 20px;
-    background: #fff;
-    display: inline-block;
-    margin: 0 4px;
-    position: relative;
-    animation: blink 6s linear infinite;
-
-    &::after {
-      content: '';
-      display: block;
-      width: 15px;
-      height: 20px;
-      border-radius: 10px;
-      background: $font-color;
-      position: absolute;
-      animation: eye 5s linear 1s infinite alternate;
-    }
-  }
-
-  @keyframes blink {
-    0% {
-      transform: scale(0);
-    }
-    5% {
-      transform: scale(1);
-    }
-    100% {
-      transform: scale(1);
-    }
-
-  }
-
-  @keyframes eye {
-    0% {
-      top: 10px;
-      left: 0;
-    }
-    5% {
-      top: 10px;
-      left: 15px;
-    }
-    20% {
-      top: 10px;
-      left: 15px;
-    }
-    25% {
-      top: 0;
-      left: 15px;
-    }
-    50% {
-      top: 0;
-      left: 15px;
-    }
-    55% {
-      top: 10px;
-      left: 15px;
-    }
-    80% {
-      top: 10px;
-      left: 15px;
-    }
-    85% {
-      top: 10px;
-      left: 0;
-    }
-    100% {
-      top: 10px;
-      left: 0;
-    }
-  }
-
-  .fade-enter-active {
-    transition: opacity .8s;
-  }
-
-  .fade-leave-active {
-    transition: opacity .2s;
-  }
-
-  .fade-enter, .fade-leave-to {
-    opacity: 0;
-  }
-
-  .fade-eye-enter-active {
-    transition: opacity .4s;
-    transition-delay: 2s;
-  }
-
-  .fade-eye-leave-active {
-    transition: opacity .1s;
-  }
-
-  .fade-eye-enter, .fade-eye-leave-to {
-    opacity: 0;
   }
 </style>

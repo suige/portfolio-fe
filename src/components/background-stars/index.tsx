7e14f95d22e92892102c9059712203d2c9159064
Kaori <suigetsukaori@gmail.com>
2023-08-20 20:09:37 -0700

    feat: replace background stars to React

---
+++ src/components/background-stars/index.tsx
@@ -0,0 +1,16 @@
+import { memo } from 'react';
+import styles from './index.module.css';
+
+const BackgroundStars = memo(() => {
+  return (
+    <div className={styles.bgStars}>
+      {[...Array(10)].map((_, index) => (
+        <div className={styles.star} key={index}>
+          ★
+        </div>
+      ))}
+    </div>
+  );
+});
+
+export { BackgroundStars };

ceccb806cc410eda8d6796c3d01c589ffbc9b6f1
Kaori <suigetsukaori@gmail.com>
2023-08-21 19:21:31 -0700

    feat: modify the contacts page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,17 @@
+import styles from './page.module.css';
+import { Steamboat } from './steamboat';
+
+export default function Contact() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.title}>Contact Me</h1>
+      <p>If you want to ask something or just say hello, please message me.</p>
+      <a href="mailto:suigetsukaori@gmail.com" className={styles.email}>
+        suigetsukaori@gmail.com
+      </a>
+      <div className={styles.imageWrapper}>
+        <Steamboat />
+      </div>
+    </main>
+  );
+}
